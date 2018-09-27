45c97baa30b2c4ac362deb95053d6f3299890170
Ivan Koryabkin <apathism@apathism.net>
2018-09-27 17:21:46 +0300

    Small optimization

--- src/EjStand/StandingBuilder.hs
+++ src/EjStand/StandingBuilder.hs
@@ -54,7 +54,7 @@ applyRunDeadline :: Maybe (UTCTime, Rational) -> Run -> (Run, Bool)
 applyRunDeadline Nothing run = (run, False)
 applyRunDeadline (Just (time, penalty)) run@Run {..}
   | runTime < time = (run, False)
-  | otherwise      = (run { runScore = runScore >>= return . (* penalty) }, True)
+  | otherwise      = (run { runScore = (* penalty) <$> runScore }, True)
 
 getRunScore :: StandingConfig -> (Run, Bool) -> Rational
 getRunScore (enableScores -> True) ((runScore -> Nothing), _)     = 0
